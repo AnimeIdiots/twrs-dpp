35a5f9ff2ec1007effc20fb2676f57d7facfaa03
Paul <paul.turkovskiy@gmail.com>
2022-07-19 01:10:15 +0300

    smth

--- cmake/FindDPP.cmake
+++ cmake/FindDPP.cmake
@@ -1,7 +1,13 @@
 find_path(DPP_INCLUDE_DIR NAMES dpp/dpp.h HINTS ${DPP_ROOT_DIR})
 
-find_library(DPP_LIBRARIES NAMES dpp "libdpp.a" HINTS ${DPP_ROOT_DIR})
+find_library(DPP_LIBRARIES NAMES dpp.lib libdpp.a HINTS ${DPP_ROOT_DIR})
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(DPP DEFAULT_MSG DPP_LIBRARIES DPP_INCLUDE_DIR)
+
+if(DPP_FOUND)
+    if(TARGET_OS STREQUAL "windows")
+        set(DPP_COPY_FILES "${PROJECT_SOURCE_DIR}/libs/DPP/bin/*.dll")
+    endif()
+endif()
